973e1106bfa3447715dec83bc89fca0fd903a022
CyDot <look@cydot.de>
2019-01-15 04:25:34 +0100

    Initial commit

---
+++ js/keep-ratio.js
@@ -0,0 +1,92 @@
+/*
+File: keep-ratio.js
+Dependencies: jQuery,
+Globals: none
+Designer: © Michael Schwarz, CyDot, look@cydot.de
+Vers. 0.0.1 
+Updated 2019-01-14
+
+-------------------------------------------
+
+ */
+
+;( function ( $ ) {
+    
+    $kRElements,
+    count = 0,
+    listening = false,
+    inertia = 333,
+    timerId,
+    cachedWidth;
+    
+    $.fn.keepRatio = function ( ratio ) {
+        var hasRatio = $.isNumeric( ratio ),
+        $els = $( this ), $el, id;
+        
+        $els.each( function () {
+            $el = $( this );
+            id = $el[ 0 ].id;
+            
+            if ( hasRatio ) {
+                $el.data( "keep-ratio", ratio );
+                if ( ! $el.hasClass( "keep-ratio" )) {
+                    $el.addClass( "keep-ratio" );
+                }
+            }
+            else if ( ! $el.data( "keep-ratio" )) {
+                $el.data( "keep-ratio", $el.width() / $el.height());
+                $el.addClass( "keep-ratio" );
+            }
+        });
+        
+        $kRElements = $( window ).find( ".keep-ratio" );
+        $els.ratio( ratio );
+        
+        if ( ! listening && $els.size()) {
+            $( window ).resize( inertiaDelay );
+        }
+    }
+    
+    $.fn.freeRatio = function () {
+        $els = $( this );
+        
+        $els.removeClass( "keep-ratio" );
+        $els.data( "keep-ratio", undefined );
+        $els.removeData( "keep-ratio" );
+    }
+    
+    $.fn.ratio = function ( ratio ) {
+        $els = $( this ), $el;
+        
+        if ( ratio === undefined ) {
+            $els.each( function () {
+                $el = $( this );
+                ratio = $el.data( "keep-ratio" );
+                if ( ratio ) $el.height( $el.width() / ratio );
+            })
+        }
+        else {
+            $els.each( function () {
+                $el = $( this );
+                $el.height( $el.width() / ratio );
+            })
+        }
+        return $els;
+    }
+    
+    function inertiaDelay () {
+        clearTimeout( timerId );
+        cachedWidth = $( window ).width();
+        if ( inertia ) {
+            timerId = setTimeout( inertiaProof, inertia )
+        }
+        else $kRElements.ratio();
+    }
+    
+    function inertiaProof () {
+        if ( cachedWidth === $( window ).width()) {
+            $kRElements.ratio();
+        }
+    }
+    
+})( jQuery );
